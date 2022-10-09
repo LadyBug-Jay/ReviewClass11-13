c183d06415ff5f684a06a954604ecd933ba5382d
Judith Elemo <98330949+LadyBug-Jay@users.noreply.github.com>
2022-10-09 01:45:48 +0100

    Update StringMethods1.java

--- src/com/syntax/reviewclass/StringMethods1.java
+++ src/com/syntax/reviewclass/StringMethods1.java
@@ -4,8 +4,12 @@ public class StringMethods1 {
     public static void main(String[] args) {
         //Method: toUpperCase and toLowerCase
 
-      String str= "hello";
-      str=str.toUpperCase();
+      String str="hello";
+      str.toUpperCase();//Because of string immutability we can not change the string, this does not have a variable. the object which is upper case "HELLO"is created but was not assigned to any variable hence java will not print the upper case
+        System.out.println(str);
+        System.out.println("-----------------------------");
+
+        str=str.toUpperCase();//Here we have reassigned the variable "str" hence java will be able to print to upper case
         System.out.println(str);
 
 
